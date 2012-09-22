e0aa5f87eca8a96b7438db1b64370eb99a444919
Jose David <josedavid-362@hotmail.com>
2012-09-21 21:40:56 -0600

    Change-Id: I0000000000000000000000000000000000000000 Signed-off-by: Jose David <josedavid-362@hotmail.com>

--- VisualCoding/src/visualcoding/gui/PanelOperacion.java
+++ VisualCoding/src/visualcoding/gui/PanelOperacion.java
@@ -6,10 +6,13 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 
 public class PanelOperacion extends JPanel {
-	private JTextField textField;
+	private JTextField _Textovaloroperacion;
 
 	/**
+	 * @author Jose David
 	 * Create the panel.
+	 *
+	 * 
 	 */
 	public PanelOperacion() {
 		setLayout(null);
@@ -22,18 +25,18 @@ public class PanelOperacion extends JPanel {
 		lblValorDeOperacion.setBounds(26, 53, 102, 14);
 		add(lblValorDeOperacion);
 		
-		textField = new JTextField();
-		textField.setBounds(143, 50, 86, 20);
-		add(textField);
-		textField.setColumns(10);
+		_Textovaloroperacion = new JTextField();
+		_Textovaloroperacion.setBounds(143, 50, 86, 20);
+		add(_Textovaloroperacion);
+		_Textovaloroperacion.setColumns(10);
 		
 		JLabel lblPropiedadAOperar = new JLabel("Propiedad a Operar");
 		lblPropiedadAOperar.setBounds(26, 84, 102, 14);
 		add(lblPropiedadAOperar);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(143, 81, 86, 20);
-		add(comboBox);
+		JComboBox _CBpropiedad = new JComboBox();
+		_CBpropiedad.setBounds(143, 81, 86, 20);
+		add(_CBpropiedad);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(143, 112, 86, 20);
@@ -42,6 +45,8 @@ public class PanelOperacion extends JPanel {
 		JLabel lblNewLabel = new JLabel("Operacion");
 		lblNewLabel.setBounds(26, 115, 102, 14);
 		add(lblNewLabel);
+		
+		
 		
 		
 
